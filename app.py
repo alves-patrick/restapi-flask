6aceea357536b7abd7a6e4c6d9d17b0ba83bc2c9
alvespatrick <patrickalvesarts@gmail.com>
2026-05-22 08:32:49 -0300

    fix: connection refused with mongo

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask 
 from flask_restful import Resource, Api
+# Seguindo a doc: importando tudo direto da biblioteca!
+from mongoengine import connect, Document, StringField, EmailField, DateTimeField
 
 app = Flask(__name__)
 api = Api(app)
 
+# Seguindo a doc: conexão direta
+connect(
+    db='users',
+    port=27017,
+    host='mongodb',
+    username='admin',
+    password='admin'
+)
+
+# Seguindo a doc: definindo o modelo sem usar "db." na frente de nada
+class UserModel(Document):
+    cpf = StringField(required=True, unique=True)
+    first_name = StringField(require=True)
+    last_name = StringField(required=True)
+    email = EmailField(required=True)
+    birth_date = DateTimeField(required=True)
+
 class Users(Resource):
     def get(self):
-        return {"message": "user1"}
+        return UserModel.objects().to_json()
+        return json.loads(usuarios_json)
+        #return {"message": "user1"}
 
 class User(Resource):
     def post(self):
@@ -15,9 +36,8 @@ class User(Resource):
     def get(self, cpf):
         return {"message": "CPF"}
 
-api.add_resource(Users, '/Users')
-api.add_resource(User, '/User', '/user/<string:cpf>')
-
+api.add_resource(Users, '/users')
+api.add_resource(User, '/user', '/user/<string:cpf>')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
